728a3c57585e6fc2df313c264785fea0231d2537
Sina <cina_sina@yahoo.com>
2021-10-14 14:39:54 +0330

    Add LName, Phone Number, Address

--- Accounts/models.py
+++ Accounts/models.py
@@ -1,16 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from .manager import UserManager
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class User(AbstractBaseUser):
     email = models.EmailField(unique=True, max_length=255)
     first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=200)
+    phone_number = PhoneNumberField(unique=True)
+    address = models.TextField(max_length=1000)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name']
+    REQUIRED_FIELDS = 'phone_number'
 
     objects = UserManager()
 
