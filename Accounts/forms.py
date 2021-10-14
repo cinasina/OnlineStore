55d1aecf148d7fd56d3d3433555c488b85623646
Sina <cina_sina@yahoo.com>
2021-10-14 22:21:16 +0330

    Delete Phone Number PrefixWidget

--- Accounts/forms.py
+++ Accounts/forms.py
@@ -2,11 +2,10 @@ from django import forms
 from .models import User
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from phonenumber_field.modelfields import PhoneNumberField
-from phonenumber_field.widgets import PhoneNumberPrefixWidget
 
 
 class UserCreationForm(forms.ModelForm):
-    phone_number = PhoneNumberField(PhoneNumberPrefixWidget(initial='IR'))
+    phone_number = PhoneNumberField()
     password = forms.CharField(label='Password', widget=forms.PasswordInput)
 
     class Meta:
