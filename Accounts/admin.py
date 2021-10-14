f14e61d3cb05db0e0b2655ca9cb90a4bfbb88cf7
Sina <cina_sina@yahoo.com>
2021-10-14 19:42:43 +0330

    Add Some Changes In List Display And Editable

--- Accounts/admin.py
+++ Accounts/admin.py
@@ -8,11 +8,11 @@ from django.contrib.auth.models import Group
 class UserAdmin(BaseUserAdmin):
     form = UserChangeForm
     add_form = UserCreationForm
-    list_display = ('email', 'first_name', 'last_name', 'phone_number',)
-    list_editable = ('email', 'phone_number', 'is_active',)
+    list_display = ('email', 'first_name', 'last_name', 'phone_number', 'is_active',)
+    list_editable = ('phone_number', 'is_active',)
     list_filter = ('is_admin', 'is_active',)
     search_fields = ('email', 'first_name', 'last_name', 'phone_number',)
-    ordering = ('date_joined',)
+    ordering = ('email',)
     fieldsets = (
         (None, {'fields': ('email', 'phone_number', 'password',)}),
         ('Personal Info', {'fields': ('first_name', 'last_name', 'address',)}),
