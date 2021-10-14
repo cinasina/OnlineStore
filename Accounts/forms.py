c7548170d59ae6d3f24783f77966e9f6bc638b99
Sina <cina_sina@yahoo.com>
2021-10-14 14:35:46 +0330

    Add Phone Number Field

--- Accounts/forms.py
+++ Accounts/forms.py
@@ -1,14 +1,17 @@
 from django import forms
 from .models import User
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from phonenumber_field.modelfields import PhoneNumberField
+from phonenumber_field.widgets import PhoneNumberPrefixWidget
 
 
 class UserCreationForm(forms.ModelForm):
+    phone_number = PhoneNumberField(widget=PhoneNumberPrefixWidget(initial='IR'))
     password = forms.CharField(label='Password', widget=forms.PasswordInput)
 
     class Meta:
         model = User
-        fields = ('email',)
+        fields = ('email', 'password', 'phone_number')
 
 
 class UserChangeForm(forms.ModelForm):
@@ -16,4 +19,4 @@ class UserChangeForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ('email', 'password', 'first_name', 'is_active', 'is_admin',)
+        fields = '__all__'
