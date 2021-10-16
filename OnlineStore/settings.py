64089f164bc864a800c9d2b8b2c886b2f7eacb2e
Sina <cina_sina@yahoo.com>
2021-10-16 12:35:26 +0330

    Add Home App

--- OnlineStore/settings.py
+++ OnlineStore/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Accounts.apps.AccountsConfig',
+    'Home.apps.HomeConfig',
     'phonenumber_field',
 ]
 
