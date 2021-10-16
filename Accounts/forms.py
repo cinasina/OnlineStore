9b66e4de415a097b009fe7c5cd577ce3666ede66
Sina <cina_sina@yahoo.com>
2021-10-16 18:09:19 +0330

    Add User Login/Register Form

--- Accounts/forms.py
+++ Accounts/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import User
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from phonenumber_field.modelfields import PhoneNumberField
+from phonenumber_field.widgets import PhoneNumberPrefixWidget
 
 
 class UserCreationForm(forms.ModelForm):
@@ -19,3 +20,28 @@ class UserChangeForm(forms.ModelForm):
     class Meta:
         model = User
         fields = '__all__'
+
+
+class UserLoginForm(forms.Form):
+    email = forms.EmailField(label='', widget=forms.EmailInput(
+        attrs={'class': "un", "type": "text", "align": "center", "placeholder": "Email"}))
+    password = forms.CharField(label='', widget=forms.PasswordInput(
+        attrs={"class": "pass", "type": "password", "align": "center",
+               "placeholder": "Password"}))
+
+
+class UserRegisterForm(forms.ModelForm):
+    email = forms.EmailField(label='', widget=forms.EmailInput(
+        attrs={'class': "un", "type": "text", "align": "center", "placeholder": "Email"}))
+    password = forms.CharField(label='', widget=forms.PasswordInput(
+        attrs={"class": "pass", "type": "password", "align": "center",
+               "placeholder": "Password"}))
+
+    class Meta:
+        model = User
+        fields = 'phone_number',
+        widgets = {'phone_number': PhoneNumberPrefixWidget(
+            initial='IR',
+            attrs={"class": "pass",
+                   "align": "center", "placeholder": "Phone Number"})}
+        labels = {'phone_number': ''}
