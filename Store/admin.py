a1dcc09f449aa359c88b97d4dc541e6abf6b0e47
Sina <cina_sina@yahoo.com>
2021-10-17 13:36:20 +0330

    Add Category And Product Admin

--- Store/admin.py
+++ Store/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    ordering = ('-created',)
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name', 'slug',)
+    list_filter = ('name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('category', 'name', 'price', 'available',)
+    list_editable = ('name', 'price', 'available',)
+    search_fields = ('category', 'name', 'slug',)
+    list_filter = ('category', 'name', 'available',)
