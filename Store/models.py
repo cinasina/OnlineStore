24a714c3294fe6d7829fff97b6f302592097edb2
Sina <cina_sina@yahoo.com>
2021-10-17 13:35:54 +0330

    Add Category And Product Model

--- Store/models.py
+++ Store/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    name = models.CharField(max_length=250)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+    price = models.PositiveSmallIntegerField(default=0)
+    image = models.ImageField(upload_to='Products/%Y/%m/%d')
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return self.name
