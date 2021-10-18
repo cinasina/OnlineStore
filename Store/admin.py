150fc1747582c51746a9d15d758d12dced7282f0
Sina <cina_sina@yahoo.com>
2021-10-18 19:23:06 +0330

    Add Unavailable Action For Product

--- Store/admin.py
+++ Store/admin.py
@@ -11,9 +11,18 @@ class CategoryAdmin(admin.ModelAdmin):
     list_filter = ('name',)
 
 
+@admin.action(description='Make products unavailable')
+def make_unavailable(product, request, queryset):
+    queryset.update(available=False)
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('category', 'name', 'price', 'available',)
     list_editable = ('name', 'price', 'available',)
+    prepopulated_fields = {'slug': ('name',)}
     search_fields = ('category', 'name', 'slug',)
     list_filter = ('category', 'name', 'available',)
+    actions = (make_unavailable,)
+
+
