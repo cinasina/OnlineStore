71564a0640b69501bdaa48618cca7c82265b2b74
Sina <cina_sina@yahoo.com>
2021-10-18 13:34:05 +0330

    Add Contact Form

---
+++ Home/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from .models import Contact
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = '__all__'
+        widgets = {
+            'name': forms.TextInput(
+                attrs={"type": "text", "class": "form-control", "name": "name", "id": "name",
+                       "placeholder": "Name"}),
+            'subject': forms.TextInput(
+                attrs={"type": "subject", "class": "form-control", "name": "subject", "id": "subject",
+                       "placeholder": "Subject"}),
+            'email': forms.EmailInput(
+                attrs={"type": "email", "class": "form-control", "name": "email", "id": "email",
+                       "placeholder": "Email"}),
+            'message': forms.TextInput(
+                attrs={"type": "message", "class": "form-control", "name": "message", "id": "message", "cols": "30",
+                       "rows": "4", "placeholder": "Message"}),
+        }
