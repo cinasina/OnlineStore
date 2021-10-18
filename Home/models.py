31bfc7d4c8d0fc9c288415ef471041b2cbeaf00c
Sina <cina_sina@yahoo.com>
2021-10-18 13:34:33 +0330

    Add Contact Model

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=150)
+    subject = models.CharField(max_length=200)
+    email = models.EmailField()
+    message = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return '{} With This Email: {} Has Something To Say!'.format(self.name, self.email)
+
+
