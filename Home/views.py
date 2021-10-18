f8c2255fa6eab148aaa652f44c9312bb146a325a
Sina <cina_sina@yahoo.com>
2021-10-18 13:35:40 +0330

    Add Contact View

--- Home/views.py
+++ Home/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+from django.contrib import messages
 
 
 def home(request):
     return render(request, 'home/home.html')
+
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Thank You! Your Message Has Been Sent', extra_tags='success')
+            return redirect('home:home')
+    else:
+        form = ContactForm()
+    return render(request, 'home/contact.html', {'form': form})
+
