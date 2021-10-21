d6b07dc999d0ba22b2d7f977f756f2aace0223be
Sina <cina_sina@yahoo.com>
2021-10-21 11:24:32 +0330

    Add Product Path In Main Urls.py

--- OnlineStore/urls.py
+++ OnlineStore/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', include('Home.urls', namespace='home')),
     path('accounts/', include('Accounts.urls', namespace='accounts')),
+    path('product/', include('Store.urls', namespace='store')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
