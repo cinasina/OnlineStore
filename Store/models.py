3d77447b587c72f04edd4ad5aff306dacdb8c35f
Sina <cina_sina@yahoo.com>
2021-10-21 11:26:18 +0330

    Add Get Absolute Url In Product Model

--- Store/models.py
+++ Store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.shortcuts import reverse
 
 
 class Category(models.Model):
@@ -30,3 +31,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('store:product', args=[self.slug])
