09875e8f53b546ef7070ab27688d93c3742d58f5
Sina <cina_sina@yahoo.com>
2021-10-30 11:40:30 +0330

    Add Category,Brand, Product Path In URL Store

--- Store/urls.py
+++ Store/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'store'
 
 urlpatterns = [
-    path('<slug:slug>/', views.details_product, name='product'),
+    path('<str:name>/', views.product_category, name='product_category'),
+    path('<str:name>/<slug:slug>/', views.product_brands, name='product_brands'),
+    path('<str:name>/<slug:slug>/<int:pk>/', views.product_single, name='product_single'),
 ]
