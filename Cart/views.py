37c0df48a3f198869a67bc5d8c0e5f9aa345e3dd
Sina <cina_sina@yahoo.com>
2021-11-06 13:59:49 +0330

    Add Cart, Add_Product, Remove_Product Views

--- Cart/views.py
+++ Cart/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .cart import Cart
+from Store.models import Product
+from Store.forms import QuantityForm
 
-# Create your views here.
+
+def cart(request):
+    user_cart = Cart(request)
+    context = {'cart': user_cart}
+    return render(request, 'cart/cart.html', context=context)
+
+
+def add_product(request, product_id):
+    user_cart = Cart(request)
+    product = get_object_or_404(Product, pk=product_id)
+    form = QuantityForm(request.POST)
+    if form.is_valid():
+        quantity = form.cleaned_data['quantity']
+        user_cart.add_product(product=product, quantity=quantity)
+        return redirect('cart:cart_details')
+
+
+def remove_product(request, product_id):
+    user_cart = Cart(request)
+    product = get_object_or_404(Product, pk=product_id)
+    user_cart.remove_product(product)
+    return redirect('cart:cart_details')
+
+
