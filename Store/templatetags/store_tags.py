d2fa317636a23bb5a392d86cad8f52e4dc14fa90
Sina <cina_sina@yahoo.com>
2021-11-30 12:17:47 +0330

    Add Custom Store Tag

--- Store/templatetags/store_tags.py
+++ Store/templatetags/store_tags.py
@@ -1,3 +1,11 @@
 from django import template
+from Store.models import Product
+
 
 register = template.Library()
+
+
+@register.inclusion_tag('store/related_product.html')
+def related_post(sub_category):
+    products = Product.objects.filter(sub_category=sub_category)[:1]
+    return {'products': products}
