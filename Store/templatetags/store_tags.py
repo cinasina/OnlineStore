72a3510fa0945a321bb0eef2b2a44af9f5b75dc8
Sina <cina_sina@yahoo.com>
2021-12-04 13:19:39 +0330

    Add Related Product Category Tag

--- Store/templatetags/store_tags.py
+++ Store/templatetags/store_tags.py
@@ -6,6 +6,6 @@ register = template.Library()
 
 
 @register.inclusion_tag('store/related_product.html')
-def related_post(sub_category):
-    products = Product.objects.filter(sub_category=sub_category)[:1]
+def related_product(sub_category, name=None):
+    products = Product.objects.filter(sub_category=sub_category).exclude(sub_category=sub_category, name=name)[:1]
     return {'products': products}
