80f94118426b35c4040d4831f0407dde6f84cdd1
Sina <cina_sina@yahoo.com>
2021-12-04 13:18:01 +0330

    Add Number Of Each Category in Product Category View

--- Store/views.py
+++ Store/views.py
@@ -6,7 +6,11 @@ from .forms import QuantityForm
 def product_category(request, name):
     categories = Category.objects.filter(is_subname=True, main_name__name__contains=name)
     products = Product.objects.filter(main_category__name__contains=name)
-    context = {'categories': categories, 'products': products}
+    product_all = Product.objects.all()
+    num_cat = {}
+    for category in product_all:
+        num_cat[category.sub_category] = num_cat.get(category.sub_category, 0) + 1
+    context = {'categories': categories, 'products': products, 'num_cat': num_cat}
     return render(request, 'store/category_product.html', context=context)
 
 
