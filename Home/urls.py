bb0df2415015c4efdbca37dda396dd9c02280701
Sina <cina_sina@yahoo.com>
2021-12-12 13:11:08 +0330

    Add News-Letter Url

--- Home/urls.py
+++ Home/urls.py
@@ -6,4 +6,5 @@ app_name = 'home'
 urlpatterns = [
     path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
+    path('letter/', views.news_letters, name='news_letter'),
 ]
