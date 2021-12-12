89f82ccb15a17552dfc736a2a3e4f75f9f56b661
Sina <cina_sina@yahoo.com>
2021-12-12 13:13:10 +0330

    Add News-Letter Model In Admin

--- Home/admin.py
+++ Home/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Contact
+from .models import Contact, NewsLetter
 
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'subject',)
+
+
+admin.site.register(NewsLetter)
