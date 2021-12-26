3024d7929a94b5b7283c7ed9e8ae0bda9c9c2792
Sina <cina_sina@yahoo.com>
2021-12-26 14:38:52 +0330

    Add Captcha Path In Main URL

--- OnlineStore/urls.py
+++ OnlineStore/urls.py
@@ -37,6 +37,7 @@ urlpatterns = [
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path('robots.txt', include('robots.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
+    path('captcha/', include('captcha.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
