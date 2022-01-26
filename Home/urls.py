57f30aa5675d498f6ed8e2d1d854371a8943e57d
Sina <cina_sina@yahoo.com>
2022-01-26 10:41:59 +0330

    Remove Coming Soon Url Path In Home Urls

--- Home/urls.py
+++ Home/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = 'home'
 
 urlpatterns = [
-    re_path(r'^', views.coming_soon, name='coming_soon'),
     path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
     path('letter/', views.news_letters, name='news_letter'),
